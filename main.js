ac67a27e023dead5d1c7774cb375bc9445ba58cc
Shuhei Ota <shuhei.ota.so@gmail.com>
2023-11-21 23:22:22 -0800

    initial commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
+  <h1>Hello World</h1>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
