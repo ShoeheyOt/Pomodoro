b7eef8372ef3183a5268fba71e3186d84492bdeb
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-03-30 12:41:14 -0700

    feat:implement 25min countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import { handleClickStart, handleClickStop } from "./timer";
+import { useState } from "react";
+
+let intervalId: number | null;
 
 function App() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+
+  const handleClickStart = () => {
+    const start = Date.now();
+    const twentyFiveMinutes = 1000 * 60 * 25;
+    const goal = start + twentyFiveMinutes;
+    intervalId = setInterval(() => {
+      setMinutes(new Date(goal - Date.now()).getMinutes());
+      setSeconds(new Date(goal - Date.now()).getSeconds());
+    }, 100);
+  };
+
+  const handleClickStop = () => {
+    intervalId && clearInterval(intervalId);
+    intervalId = null;
+  };
+
+  const handleClickReset = () => {
+    intervalId && clearInterval(intervalId);
+    setMinutes(25);
+    setSeconds(0);
+    intervalId = null;
+  };
   return (
     <>
       <header className="text-6xl">Pomodoro Timer</header>
       <main>
         <div className="flex justify-center gap-2 text-6xl mt-14 mb-5">
-          <div>2</div>
-          <div>5</div>
+          <div>{minutes}</div>
           <div>:</div>
-          <div>0</div>
-          <div>0</div>
+          <div>{seconds}</div>
         </div>
       </main>
-      <button className="mr-5" onClick={handleClickStart}>
-        Start
-      </button>
-      <button onClick={handleClickStop}>Stop</button>
+      <button onClick={handleClickStart}>start</button>
+      <button onClick={handleClickStop}>stop</button>
+      <button onClick={handleClickReset}>Reset</button>
     </>
   );
 }
